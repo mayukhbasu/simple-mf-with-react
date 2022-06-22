d861fda2ff7fcbb47da5eec968414871b6ff2f96
Bubul <bubul@Bubuls-MBP.Dlink>
2022-06-22 15:36:01 +0530

    added problem description

--- page1/src/App.jsx
+++ page1/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Details from "./Details";
 
 import "./index.css";
 
 const App = () => (
   <div className="container">
-    <div>Name: page1</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Empty CSS</div>
+    <div className="row">
+      <Details/>
+    </div>
+    
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ readme.md
@@ -0,0 +1,8 @@
+Task:
+1. Create page1 and page2 two microfrontends based on react
+2. Create a component Details in page1 application
+3. Form on page1 should store data inside indexed db
+4. Display all data on page2 application
+5. Create a delete button beside every row to remove data
+6. Create a delete all button inside page 2
+7. If all records are deleted come back to page 1
